aceab616935221701436f8bac82e8b21b798e787
livehass <silva.felipe12@hotmail.com>
2023-06-19 13:21:45 -0300

    adiciona poto e virgula

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -2,5 +2,5 @@ const lista = document.querySelector("[data-lista]");
 
 function constroiCard(){
     const video = document.createElement("li");
-    video.className = "videos__item"
+    video.className = "videos__item";
 }
