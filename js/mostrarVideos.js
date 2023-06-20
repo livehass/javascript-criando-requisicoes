ce36398610012068ab3179676c6d884e0885f896
livehass <silva.felipe12@hotmail.com>
2023-06-20 11:37:23 -0300

    ultimo commit

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -6,7 +6,7 @@ function constroiCard(titulo, descricao, url, imagem){
     const video = document.createElement("li");
     video.className = "videos__item";
     video.innerHTML = `            
-    <iframe width="100%" height="72%" src="${url}"
+    <iframe width="100%" height="60%" src="${url}"
         title="${titulo}" frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen></iframe>
