2629b97c85620d4f2a50bddd17408fd0f60de5f4
livehass <silva.felipe12@hotmail.com>
2023-06-21 11:55:57 -0300

    altera conexão da api

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -4,13 +4,25 @@ async function listaVideos(){
     
     return conexaoConvertida;
 }
-async function criaVideo(){
-    const conexaoVideo = await fetch("http://localhost:3000/videos", {
+async function criaVideo(titulo, descricao, url, imagem){
+    const conexao = await fetch("http://localhost:3000/videos", {
         method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
     });
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
 }
 
 export const conectaApi = {
-    listaVideos
+    listaVideos,
+    criaVideo
 }
 
