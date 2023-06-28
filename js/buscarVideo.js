00811986522c8c63342148e65ee05be999a2074c
livehass <silva.felipe12@hotmail.com>
2023-06-28 12:07:15 -0300

    adiciona novo export default

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -1,9 +1,10 @@
 import { conectaApi } from "./conectaApi.js";
 
-async function buscarVideo(){
+async function buscarVideo(evento){
+    evento.preventDedfault();
     const dadosDePesquisa = document.querySelector("[data-pesquisa]");
     const busca = await conectaApi.buscaVideo(dadosDePesquisa);
 }
 
 const botaoDePesquisa = document.querySelector("data-botao-pesquisa");
-botaoDePesquisa.addEventListener("submit",)
+botaoDePesquisa.addEventListener("click", evento => buscarVideo(evento));
